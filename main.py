9bdd4654837babfad153b024c870ba08c3d0fb17
Yi Cao <caoyi06@qq.com>
2022-07-08 20:44:06 +0800

    c

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def read_start():
         return int(f.read().strip())
 
 def save_start(start):
-    logging.info("Saving new start {} to start.txt".format(start))
+    print("Saving new start {} to start.txt".format(start))
     with open("start.txt", "w") as f:
         f.write(str(start))
 
@@ -19,16 +19,16 @@ def get_xkcd_data(num):
     http = urllib3.PoolManager()
     response = http.request("GET", url)
     if response.status != 200:
-        logging.info("Failed to fetch data from {}".format(url))
+        print("Failed to fetch data from {}".format(url))
         return None
-    logging.info("Fetched data from {}".format(url))
+    print("Fetched data from {}".format(url))
     return json.loads(response.data.decode("utf-8"))
 
 def get_friend_list():
     http = urllib3.PoolManager()
     response = http.request("GET", COOLQ_ENDPOINT.format("get_friend_list"))
     if response.status != 200:
-        logging.error("Failed to fetch friend list")
+        print("Failed to fetch friend list")
         return None
     return json.loads(response.data.decode("utf-8"))
 
@@ -40,9 +40,9 @@ def do_send(user_id, image):
             )
     ))
     if response.status != 200:
-        logging.error("Failed to send image to {}".format(user_id))
+        print("Failed to send image to {}".format(user_id))
         return
-    logging.info("Sent image to {}".format(user_id))
+    print("Sent image to {}".format(user_id))
 
 def action():
     curr = read_start()
@@ -50,19 +50,19 @@ def action():
     if comic_data is None:
         return
     save_start(curr + 1)
-    logging.info("Sending data to CoolQ")
+    print("Sending data to CoolQ")
     for friend in get_friend_list()["data"]:
         do_send(friend["user_id"], comic_data["img"])
     
 
 if __name__ == "__main__":
-    logging.info("Starting...")
-    logging.info("Using COOLQ_ENDPOINT: {}".format(COOLQ_ENDPOINT))
-    logging.info("Using XKCD_ENDPOINT: {}".format(XKCD_ENDPOINT))
+    print("Starting...")
+    print("Using COOLQ_ENDPOINT: {}".format(COOLQ_ENDPOINT))
+    print("Using XKCD_ENDPOINT: {}".format(XKCD_ENDPOINT))
     while True:
         try:
             action()
         except Exception as e:
-            logging.error("Exception: {}".format(e))
+            print("Exception: {}".format(e))
         time.sleep(POOLING_INTERVAL)
         
